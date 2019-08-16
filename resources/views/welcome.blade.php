6d444966757e444ce41498f5d88061d30992affc
munna-kolpolok <munna.kolpolok@gmail.com>
2019-08-16 20:15:20 +0600

    sorting bug fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,24 +12,6 @@
 
         <link href="{{ asset('la-assets/css/AdminLTE.css') }}" rel="stylesheet" type="text/css" />
 
-        <style type="text/css">
-            .card {
-              /* Add shadows to create the "card" effect */
-              box-shadow: 0 4px 4px 0 rgba(0,0,0,0.2);
-              transition: 0.3s;
-              margin-bottom: 5px; 
-            }
-
-            /* On mouse-over, add a deeper shadow */
-            .card:hover {
-              box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-            }
-
-            /* Add some padding inside the card container */
-            .container {
-              padding: 2px 16px;
-            }
-        </style>
 
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <style>
